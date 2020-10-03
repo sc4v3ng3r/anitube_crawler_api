9eed3dc866e48143b6812b77ca9fdda03eabb758
Boaventura <scavenger.gnu@gmail.com>
2020-10-02 23:12:15 -0300

    :fire: removing useless code.

--- test/mock_data/paths.dart
+++
@@ -1,5 +0,0 @@
-const MOCKED_GENRES_HTML_PAGE =
-    "./test/mock_data/anitube_pages/genres_page.html";
-
-const MOCKED_ANIME_DETAILS_HTML_PAGE =
-    "./test/mock_data/anitube_pages/anime_details_page.html";
